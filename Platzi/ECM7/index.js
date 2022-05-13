91c93f02a2b538d8db2c849dbaf0d74e51112f34
carlosdiazz <c.diazadriann@gmail.com>
2022-05-13 08:21:13 -0400

    c

--- Platzi/ECM7/index.js
+++ Platzi/ECM7/index.js
@@ -3,7 +3,7 @@
 const holaMundo = () => {
     return new Promise((resolve, reject) => {
 
-        (true)
+        (false)
         ? setTimeout(() => resolve('Hola Mundo'), 3000)
         : reject(new Error('Test Error'))
     });
@@ -21,3 +21,21 @@ const holaMundoAsync = async () => {
 }
 
 holaMundoAsync()
+
+
+holaMundo()
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finalizado'))
+
+
+//! Flat en arreglos
+const arreglo = [1,2,3,[1,2,3,[1,2,3]]]
+
+//! Aqui convierto arreglos en un solo arreglo
+console.log(arreglo.flat(2))
+
+const arreglo2 = [1,2,3,4,5,6,7,8,9,10]
+//! Aqui puedo recorrer un arreglo y aplicar una funcion a cada elemento
+console.log(arreglo2.flatMap(numero => [numero, numero * 2]))
+
